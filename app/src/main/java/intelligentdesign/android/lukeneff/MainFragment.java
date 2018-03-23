4ed38976a63883c7dffcdd396717293ebd0695d8
BtLutz <brian.lutz9@icloud.com>
2018-03-23 13:05:29 -0400

    Adding code to inflate the layout for a fragment. Next up is the camera intent.

--- app/src/main/java/intelligentdesign/android/lukeneff/MainFragment.java
+++ app/src/main/java/intelligentdesign/android/lukeneff/MainFragment.java
@@ -1,6 +1,11 @@
 package intelligentdesign.android.lukeneff;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -11,4 +16,13 @@ import android.widget.ImageView;
 public class MainFragment extends Fragment {
     private ImageView mPhotoTaken;
     private Button mTakePhoto;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_main, container, false);
+        mPhotoTaken = (ImageView) v.findViewById(R.id.photo_taken);
+        mTakePhoto = (Button) v.findViewById(R.id.take_photo_button);
+        return v;
+    }
 }
