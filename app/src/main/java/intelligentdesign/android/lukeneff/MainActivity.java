9715c6ea08ab484482f7c07b0b03e5b407561e6c
BtLutz <brian.lutz9@icloud.com>
2018-03-23 14:11:41 -0400

    I missed the line originally to initialize the view of our MainActivity; Including a line to set the View of the main activity fixed our issues.

--- app/src/main/java/intelligentdesign/android/lukeneff/MainActivity.java
+++ app/src/main/java/intelligentdesign/android/lukeneff/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         FragmentManager fm = getSupportFragmentManager();
         Fragment f = fm.findFragmentById(R.id.fragment_container);
         if (f == null) {
