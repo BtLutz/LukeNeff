4be6e890bb17c99c846b3fe5e6cc79490f8ad0c9
ekschro <ekschro@protonmail.com>
2018-03-23 14:57:34 -0400

    Added google play services.

--- app/src/main/java/intelligentdesign/android/lukeneff/MainFragment.java
+++ app/src/main/java/intelligentdesign/android/lukeneff/MainFragment.java
@@ -9,19 +9,21 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.android.gms.vision.CameraSource;
 /**
  * Created by brianlutz on 3/23/18.
  */
 
 public class MainFragment extends Fragment {
-    private ImageView mPhotoTaken;
+    private ImageView mCameraSource = null;
+
     private Button mTakePhoto;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_main, container, false);
-        mPhotoTaken = (ImageView) v.findViewById(R.id.photo_taken);
+        mCameraSource = (ImageView) v.findViewById(R.id.preview);
         mTakePhoto = (Button) v.findViewById(R.id.take_photo_button);
         return v;
     }
