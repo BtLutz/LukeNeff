7350794b541426dc0958cf47e030cc6c16b9b283
ekschro <ekschro@protonmail.com>
2018-04-04 23:26:54 -0400

    Fixed refactoring error

--- app/src/main/java/intelligentdesign/android/lukeneff/FaceEvaluateFragment.java
+++ app/src/main/java/intelligentdesign/android/lukeneff/FaceEvaluateFragment.java
@@ -171,7 +171,7 @@ public class FaceEvaluateFragment extends Fragment {
         String key = mDatabaseReference.child("data-point").push().getKey();
 
         // Building the new DataPoint object for FireBase
-        History fBDaFtaPoint = new History(happiness);
+        History fBDataPoint = new History(happiness);
         Map<String, Object> dataPointValues = fBDataPoint.toMap();
 
         // How we'll be putting stuff into the database. This object shows the path for where
